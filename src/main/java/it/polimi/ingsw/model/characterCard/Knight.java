c0ec092d3cdb021ce4ccd823bde7818d9d1fda0e
Gabriele Munafo <gabriele.munafo@mail.polimi.it>
2022-05-07 15:31:33 +0200

    Add Knight and KnightTest

---
+++ src/main/java/it/polimi/ingsw/model/characterCard/Knight.java
@@ -0,0 +1,210 @@
+package it.polimi.ingsw.model.characterCard;
+
+import it.polimi.ingsw.exceptions.IslandException;
+import it.polimi.ingsw.model.House;
+import it.polimi.ingsw.model.Island;
+import it.polimi.ingsw.model.Player;
+
+import java.util.Map;
+
+/**
+ * Class that represent the Knight character card
+ * @author Gabriele Munafò
+ */
+public class Knight extends CharacterCard {
+
+    private Player currentPlayer = null;
+
+    public Knight(int cost) {
+        super(cost, "Knight");
+    }
+
+    /**
+     * Calculates the influence on a certain island, using a different version of checkInfluence
+     * @param parameters the map containing the island where to calculate the influence
+     * @throws NullPointerException when parameters is null or when island is null
+     */
+    @Override
+    public void doEffect(Map<String, Object> parameters) throws Exception{
+        super.doEffect(parameters);
+
+        if (parameters == null){
+            throw new NullPointerException();
+        }
+        currentPlayer = (Player) parameters.get("CurrentPlayer");
+        if (currentPlayer == null){
+            throw new NullPointerException();
+        }
+    }
+
+
+
+    /**
+     * Calculates the influence on a certain island, giving 2 extra points to the player who played the card
+     * @param island where to calculate the influence
+     * @param expertMode the mode the game is in
+     * @param numPlayers the number of players
+     * @param arrayPlayers the array of players
+     * @return the player with highest influence
+     */
+    @Override
+    public Player checkInfluence(Island island, Boolean expertMode, int numPlayers, Player[] arrayPlayers) {
+        if (expertMode && island.isNoEntryTilePresent()){
+            return(null);
+        }
+        int[] influences;
+
+            if (numPlayers == 2 || numPlayers == 3) {
+            influences = new int[numPlayers];
+            for (int i = 0; i < numPlayers; i++) {
+                influences[i] = 0;
+                if (arrayPlayers[i].equals(currentPlayer)) {
+                    influences[i] = influences[i] + 2;
+                }
+                for (House house : House.values()) {
+                    if (arrayPlayers[i].getDashboard().isProfPresent(house)) {
+                        influences[i] = influences[i] + island.getHouseStudents(house);
+                    }
+                }
+                try{
+                    if ((arrayPlayers[i].getDashboard().getTowerColor()).equals(island.getColorTower())) {
+                        influences[i] = influences[i] + island.getNumTower();
+                    }
+                }
+                catch (IslandException e){
+                    influences[i] = influences[i];
+                }
+
+            }
+
+            int max = 0;
+            boolean tie = false;
+
+            for (int i = 1; i < numPlayers; i++) {
+                if (influences[max] <= influences[i]) {
+                    if (influences[max] == influences[i]) {
+                        tie = true;
+                    } else {
+                        tie = false;
+                        max = i;
+                    }
+                }
+            }
+            if (tie) {
+                return (null);
+            } else {
+                return (arrayPlayers[max]);
+            }
+        }
+        //four players
+            else {
+            influences = new int[2];
+            int q = 0;
+            int teamOneLeader = 0;
+            int teamTwoLeader = 0;
+            int[] teamOne = new int[2];
+            int[] teamTwo = new int[2];
+
+            for(int i=0; i<numPlayers; i++) {
+                if(arrayPlayers[i].isTeamLeader() && q ==0) {
+                    teamOneLeader = i;
+                    teamOne[0] = i;
+                    q++;
+                }
+                else if(arrayPlayers[i].isTeamLeader()) {
+                    teamTwoLeader = i;
+                    teamTwo[0] = i;
+                    q = 0;
+                    break;
+                }
+            }
+
+            for(int i=0; i<numPlayers; i++) {
+                if (i != teamOneLeader && i != teamTwoLeader) {
+                    if (arrayPlayers[i].getDashboard().getTowerColor().equals(arrayPlayers[teamOneLeader].getDashboard().getTowerColor())) {
+                        teamOne[1] = i;
+                    } else {
+                        teamTwo[1] = i;
+                    }
+                }
+
+            }
+
+            boolean towerAdded = false;
+            boolean added = false;
+
+            for(int i=0; i<2; i++){
+                influences[i] = 0;
+
+                for(House h : House.values()) {
+                    if (i == 0) {
+                        if(arrayPlayers[teamOne[0]].getDashboard().isProfPresent(h)){
+                            influences[i] += island.getHouseStudents(h);
+                        }
+                        if(arrayPlayers[teamOne[1]].getDashboard().isProfPresent(h)){
+                            influences[i] += island.getHouseStudents(h);
+                        }
+                        try {
+                            if (!towerAdded && arrayPlayers[teamOne[0]].getDashboard().getTowerColor().equals(island.getColorTower())) {
+                                influences[i] = influences[i] + island.getNumTower();
+                                towerAdded = true;
+                            }
+                        }
+                        catch (IslandException e){
+                            influences[q] = influences[q];
+                        }
+
+                        if (!added && (arrayPlayers[teamOne[0]].equals(currentPlayer) || arrayPlayers[teamOne[1]].equals(currentPlayer))) {
+                            influences[i] = influences[i] + 2;
+                            added = true;
+                        }
+
+                    }
+                    if (i == 1) {
+                        if(arrayPlayers[teamTwo[0]].getDashboard().isProfPresent(h)){
+                            influences[i] += island.getHouseStudents(h);
+                        }
+                        if(arrayPlayers[teamTwo[1]].getDashboard().isProfPresent(h)){
+                            influences[i] += island.getHouseStudents(h);
+                        }
+                        try {
+                            if (!towerAdded && arrayPlayers[teamTwo[0]].getDashboard().getTowerColor().equals(island.getColorTower())) {
+                                influences[i] = influences[i] + island.getNumTower();
+                                towerAdded = true;
+                            }
+                        }
+                        catch (IslandException e){
+                            influences[q] = influences[q];
+                        }
+                        if (!added && (arrayPlayers[teamTwo[0]].equals(currentPlayer) || arrayPlayers[teamTwo[1]].equals(currentPlayer))) {
+                            influences[i] = influences[i] + 2;
+                            added = true;
+                        }
+
+                    }
+                }
+            }
+
+            int max = 0;
+            boolean tie = false;
+
+            if (influences[max] <= influences[1]) {
+                if (influences[max] == influences[1]) {
+                    tie = true;
+                } else {
+                    max = 1;
+                }
+            }
+            if (tie) {
+                return (null);
+            } else {
+                if (max == 0){
+                    return (arrayPlayers[teamOneLeader]);
+                } else {
+                    return (arrayPlayers[teamTwoLeader]);
+                }
+            }
+        }
+    }
+}
+
---
+++ src/test/java/it/polimi/ingsw/model/characterCard/KnightTest.java
@@ -0,0 +1,199 @@
+package it.polimi.ingsw.model.characterCard;
+
+import it.polimi.ingsw.exceptions.EntranceException;
+import it.polimi.ingsw.exceptions.IllegalChoiceException;
+import it.polimi.ingsw.exceptions.TowerAreaException;
+import it.polimi.ingsw.exceptions.noEntryTileException;
+import it.polimi.ingsw.model.Color;
+import it.polimi.ingsw.model.Island;
+import it.polimi.ingsw.model.Player;
+import it.polimi.ingsw.model.characterCard.Knight;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static it.polimi.ingsw.model.House.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * CentaurTest class tests Centaur class
+ *
+ * @author Gabriele Munafo'
+ * @see Knight
+ */
+class KnightTest {
+
+    private int numPlayers;
+    private boolean expertMode;
+    private Island island;
+    private Player[] arrayPlayers;
+    private Knight k;
+
+    /**
+     * Setups the knight card k which will be used in the following tests
+     */
+    @BeforeEach
+    public void setup() {
+        k = new Knight(3);
+    }
+
+    /**
+     * Verifies that the influence for 2 players is calculated correctly
+     */
+    @Test
+    void checkInfluenceTest_2Players() {
+        numPlayers = 2;
+        expertMode = false;
+        island = new Island();
+
+        arrayPlayers = new Player[numPlayers];
+        for (int i=0; i<numPlayers; i++){
+            arrayPlayers[i] = new Player(numPlayers);
+        }
+
+        arrayPlayers[0].setDashboard(Color.WHITE);
+        arrayPlayers[1].setDashboard(Color.BLACK);
+
+        island.addStudents(GREEN, 1);
+        arrayPlayers[1].getDashboard().addProf(GREEN);
+
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("CurrentPlayer", arrayPlayers[0]);
+        try {
+            k.doEffect(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Player result = k.checkInfluence(island, expertMode, numPlayers, arrayPlayers);
+        assertEquals(arrayPlayers[0], result);
+    }
+
+    /**
+     * Verifies that the influence for 2 players is calculated correctly in case of a tie
+     */
+    @Test
+    void checkInfluenceTest_2Players_Tie() {
+        numPlayers = 2;
+        expertMode = false;
+        island = new Island();
+
+        arrayPlayers = new Player[numPlayers];
+        for (int i=0; i<numPlayers; i++){
+            arrayPlayers[i] = new Player(numPlayers);
+        }
+
+        arrayPlayers[0].setDashboard(Color.WHITE);
+        arrayPlayers[1].setDashboard(Color.BLACK);
+
+        island.addStudents(GREEN, 1);
+        arrayPlayers[0].getDashboard().addProf(GREEN);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("CurrentPlayer", arrayPlayers[1]);
+        try {
+            k.doEffect(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Player result = k.checkInfluence(island, expertMode, numPlayers, arrayPlayers);
+        assertEquals(arrayPlayers[1], result);
+
+        island.addStudents(GREEN, 1);
+        result = k.checkInfluence(island, expertMode, numPlayers, arrayPlayers);
+        assertNull(result);
+    }
+
+    /**
+     * Verifies that the influence for 4 players is calculated correctly
+     */
+    @Test
+    void checkInfluenceTest_4Players() {
+        numPlayers = 4;
+        expertMode = false;
+        island = new Island();
+
+        arrayPlayers = new Player[numPlayers];
+        for (int i=0; i<numPlayers; i++){
+            arrayPlayers[i] = new Player(numPlayers);
+        }
+
+        arrayPlayers[1].setTeamLeader(true);
+        arrayPlayers[0].setTeamLeader(false);
+        arrayPlayers[2].setTeamLeader(true);
+        arrayPlayers[3].setTeamLeader(false);
+
+        arrayPlayers[0].setDashboard(Color.WHITE);
+        arrayPlayers[1].setDashboard(Color.WHITE);
+        arrayPlayers[2].setDashboard(Color.BLACK);
+        arrayPlayers[3].setDashboard(Color.BLACK);
+
+        island.addStudents(GREEN, 1);
+        arrayPlayers[2].getDashboard().addProf(GREEN);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("CurrentPlayer", arrayPlayers[0]);
+        try {
+            k.doEffect(map);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        Player result = k.checkInfluence(island, expertMode, numPlayers, arrayPlayers);
+        assertEquals(arrayPlayers[1], result);
+
+        island.addStudents(BLUE, 4);
+        arrayPlayers[3].getDashboard().addProf(BLUE);
+        island.addStudents(YELLOW, 1);
+        arrayPlayers[1].getDashboard().addProf(YELLOW);
+
+        result = k.checkInfluence(island, expertMode, numPlayers, arrayPlayers);
+        assertEquals(arrayPlayers[2], result);
+    }
+
+    /**
+     * Verifies that if the island has a noEntryTile on it, we will receive null as a result
+     */
+    @Test
+    void checkInfluenceTest_NoEntryTile() {
+        numPlayers = 2;
+        expertMode = true;
+        island = new Island();
+
+        arrayPlayers = new Player[numPlayers];
+        for (int i=0; i<numPlayers; i++){
+            arrayPlayers[i] = new Player(numPlayers);
+        }
+
+        arrayPlayers[0].setDashboard(Color.WHITE);
+        arrayPlayers[1].setDashboard(Color.BLACK);
+
+        island.addNoEntryTile();
+
+        island.addTowers(2);
+        island.setTowerColor(arrayPlayers[0].getDashboard().getTowerColor());
+
+        island.addStudents(GREEN, 1);
+
+        arrayPlayers[1].getDashboard().addProf(GREEN);
+
+        Player result = k.checkInfluence(island, expertMode, numPlayers, arrayPlayers);
+        assertNull(result);
+    }
+
+    /**
+     * Verifies that the exceptions on doEffect work properly
+     */
+    @Test
+    void doEffect_Exception(){
+        assertThrows(NullPointerException.class, ()->k.doEffect(null));
+        Map<String, Object> map = new HashMap<>();
+        map.put("CurrentPlayer", null);
+        assertThrows(NullPointerException.class, ()->k.doEffect(map));
+    }
+
+}
