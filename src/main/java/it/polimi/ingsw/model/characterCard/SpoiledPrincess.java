298702a42e71b86effa615c937c5f30a64db3204
alessio-buda <100711335+alessio-buda@users.noreply.github.com>
2022-05-08 23:23:33 +0200

    Add SpoiledPrincess and SpoiledPrincessTest classes

---
+++ src/main/java/it/polimi/ingsw/model/characterCard/SpoiledPrincess.java
@@ -0,0 +1,109 @@
+package it.polimi.ingsw.model.characterCard;
+
+import it.polimi.ingsw.exceptions.BagException;
+import it.polimi.ingsw.exceptions.NotEnoughStudentsOnCardException;
+import it.polimi.ingsw.exceptions.StudentsTableException;
+import it.polimi.ingsw.model.Bag;
+import it.polimi.ingsw.model.House;
+import it.polimi.ingsw.model.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SpoiledPrincess extends CharacterCard {
+    private Map<House, Integer> houseMap;
+
+    /**
+     * Class constructor, initializes card with name, initial cost and required students.
+     *
+     * @param bag bag from which students are pulled to initialize card
+     */
+    public SpoiledPrincess(Bag bag) throws BagException {
+        super(2, "SpoiledPrincess");
+
+        houseMap = new HashMap<>();
+        for (House h : House.values()){
+            houseMap.put(h, 0);
+        }
+        for (int i=0; i<4; i++) {
+            House h = bag.pull();
+            houseMap.replace(h, houseMap.get(h) + 1);
+        }
+    }
+
+    /**
+     * Receives parameters, saves them and calls the correct method to perform the wanted operation.
+     *
+     * @param parameters map containing the necessary parameters to perform card operations
+     * @throws IllegalArgumentException missing parameter in parameters
+     */
+    @Override
+    public void doEffect(Map<String, Object> parameters) throws IllegalArgumentException, StudentsTableException, BagException, NotEnoughStudentsOnCardException {
+        House wantedHouse;
+        HashMap<House, Integer> getHouseMap;
+        Player currentPlayer;
+        Bag bag;
+
+        List<String> moveParameters = new ArrayList<>();
+        moveParameters.add("wantedHouse");
+        moveParameters.add("currentPlayer");
+        moveParameters.add("bag");
+
+        if (!parameters.containsKey("method"))
+            throw new IllegalArgumentException("Missing parameter method");
+        if (parameters.get("method") == null)
+            throw new NullPointerException("Null parameter method");
+        String method = (String) parameters.get("method");
+        switch (method) {
+            case "getStudents":
+                Map<House, Integer> studentsOnCard = new HashMap<>(this.getStudents());
+                parameters.put("studentsOnCard", studentsOnCard);
+                break;
+            case "move":
+                for (String s : moveParameters) {
+                    if (!parameters.containsKey(s))
+                        throw new IllegalArgumentException("Missing parameter " + s);
+                    if (parameters.get(s) == null)
+                        throw new NullPointerException("Null parameter " + s);
+                }
+
+                wantedHouse = (House) parameters.get("wantedHouse");
+                currentPlayer = (Player) parameters.get("currentPlayer");
+                bag = (Bag) parameters.get("bag");
+                this.move(wantedHouse, currentPlayer, bag);
+                break;
+        }
+    }
+
+    /**
+     * Returns a map with the number of students on the card for each house.
+     *
+     * @return map containing the number of students on the card for each house.
+     */
+    private Map<House, Integer> getStudents(){
+        return new HashMap<>(houseMap);
+    }
+
+    /**
+     * Removes a student of the selected house from the card and places him in the current player's dining hall.
+     *
+     * @param wantedHouse house of the student to be removed from card
+     * @param currentPlayer current player
+     * @param bag bag needed to refill the card
+     */
+    private void move(House wantedHouse, Player currentPlayer, Bag bag) throws StudentsTableException, BagException, NotEnoughStudentsOnCardException {
+        if (houseMap.get(wantedHouse) < 1)
+            throw new NotEnoughStudentsOnCardException("Not enough students of" + wantedHouse + " house on the card");
+        House pulled = bag.pull();
+        houseMap.replace(wantedHouse, houseMap.get(wantedHouse) - 1);
+        houseMap.replace(pulled, houseMap.get(pulled) + 1);
+        currentPlayer.getDashboard().getDiningHall().addStudents(wantedHouse, 1);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Take 1 student from this card and place it in your Dining Room. Then, draw a new Student from the Bag and place it on this card";
+    }
+}
---
+++ src/test/java/it/polimi/ingsw/model/characterCard/SpoiledPrincessTest.java
@@ -0,0 +1,242 @@
+package it.polimi.ingsw.model.characterCard;
+
+import it.polimi.ingsw.exceptions.BagException;
+import it.polimi.ingsw.exceptions.NotEnoughStudentsOnCardException;
+import it.polimi.ingsw.exceptions.StudentsTableException;
+import it.polimi.ingsw.model.Bag;
+import it.polimi.ingsw.model.Color;
+import it.polimi.ingsw.model.House;
+import it.polimi.ingsw.model.Player;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * SpoiledPrincessTest class test SpoiledPrincess class.
+ *
+ * @author Alessio Buda
+ * @see SpoiledPrincess
+ */
+public class SpoiledPrincessTest {
+    private static final int numPlayers = 2;
+    private SpoiledPrincess spoiledPrincess;
+    private Player currentPlayer;
+    private Bag bag;
+
+    /**
+     * Setups test.
+     */
+    @BeforeEach
+    void setup (){
+        bag = new Bag();
+        try {
+            spoiledPrincess = new SpoiledPrincess(bag);
+        } catch (BagException e) {
+            e.printStackTrace();
+            fail();
+        }
+        currentPlayer = new Player(numPlayers);
+        currentPlayer.setDashboard(Color.WHITE);
+    }
+
+    /**
+     * Tears down test.
+     */
+    @AfterEach
+    void tearDown () {
+        spoiledPrincess = null;
+        currentPlayer = null;
+        bag = null;
+    }
+
+    /**
+     * Verifies that the students initially on the card are removed and placed in current player's dining hall.
+     */
+    @Test
+    void doEffectTest_move() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("method", "getStudents");
+        try {
+            spoiledPrincess.doEffect(parameters);
+        } catch (BagException | StudentsTableException | NotEnoughStudentsOnCardException e) {
+            e.printStackTrace();
+            fail();
+        }
+        HashMap<House, Integer> studentsOnCard = new HashMap<>((HashMap<House, Integer>) parameters.get("studentsOnCard"));
+        HashMap<House, Integer> oldStudentsOnCard = new HashMap<>(studentsOnCard);
+        for (House h : House.values()) {
+            while (studentsOnCard.get(h) > 0) {
+                studentsOnCard.replace(h, studentsOnCard.get(h)-1);
+                parameters.replace("method", "move");
+                parameters.put("wantedHouse", h);
+                parameters.put("currentPlayer", currentPlayer);
+                parameters.put("bag", bag);
+                try {
+                    spoiledPrincess.doEffect(parameters);
+                } catch (BagException | StudentsTableException | NotEnoughStudentsOnCardException e) {
+                    e.printStackTrace();
+                    fail();
+                }
+            }
+        }
+        for (House h : House.values()) {
+            assertEquals(oldStudentsOnCard.get(h), currentPlayer.getDashboard().getDiningHall().getHouseStudents(h));
+        }
+
+    }
+
+    /**
+     * Verifies that an IllegalArgumentException is thrown when "method" parameter in parameters is missing.
+     */
+    @Test
+    void doEffectTest_NoMethodParameterGiven_ThrowsIllegalArgumentException() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("wantedHouse", House.YELLOW);
+        parameters.put("currentPlayer", currentPlayer);
+        parameters.put("bag", bag);
+
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> spoiledPrincess.doEffect(parameters));
+        Assertions.assertEquals("Missing parameter method", thrown.getMessage());
+    }
+
+    /**
+     * Verifies that an NullPointerException is thrown when "method" parameter in parameters is null.
+     */
+    @Test
+    void doEffectTest_NullMethodParameterGiven_ThrowsNullPointerException() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("method", null);
+        parameters.put("wantedHouse", House.YELLOW);
+        parameters.put("currentPlayer", currentPlayer);
+        parameters.put("bag", bag);
+
+        NullPointerException thrown = Assertions.assertThrows(NullPointerException.class, () -> spoiledPrincess.doEffect(parameters));
+        Assertions.assertEquals("Null parameter method", thrown.getMessage());
+    }
+
+
+    @Test
+    void doEffectTest_NoWantedHouseParameterGiven_ThrowsIllegalArgumentException() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("method", "move");
+        parameters.put("currentPlayer", currentPlayer);
+        parameters.put("bag", bag);
+
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> spoiledPrincess.doEffect(parameters));
+        Assertions.assertEquals("Missing parameter wantedHouse", thrown.getMessage());
+    }
+
+    /**
+     * Verifies that an NullPointerException is thrown when "wantedHouse" parameter in parameters is null.
+     */
+    @Test
+    void doEffectTest_NullWantedHouseParameterGiven_ThrowsNullPointerException() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("method", "move");
+        parameters.put("wantedHouse", null);
+        parameters.put("currentPlayer", currentPlayer);
+        parameters.put("bag", bag);
+
+        NullPointerException thrown = Assertions.assertThrows(NullPointerException.class, () -> spoiledPrincess.doEffect(parameters));
+        Assertions.assertEquals("Null parameter wantedHouse", thrown.getMessage());
+    }
+
+    /**
+     * Verifies that an IllegalArgumentException is thrown when "currentPlayer" parameter in parameters is missing.
+     */
+    @Test
+    void doEffectTest_NoCurrentPlayerParameterGiven_ThrowsIllegalArgumentException() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("method", "move");
+        parameters.put("wantedHouse", House.YELLOW);
+        parameters.put("bag", bag);
+
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> spoiledPrincess.doEffect(parameters));
+        Assertions.assertEquals("Missing parameter currentPlayer", thrown.getMessage());
+    }
+
+    /**
+     * Verifies that an NullPointerException is thrown when "currentPlayer" parameter in parameters is null.
+     */
+    @Test
+    void doEffectTest_NullCurrentPlayerParameterGiven_ThrowsNullPointerException() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("method", "move");
+        parameters.put("wantedHouse", House.YELLOW);
+        parameters.put("currentPlayer", null);
+        parameters.put("bag", bag);
+
+        NullPointerException thrown = Assertions.assertThrows(NullPointerException.class, () -> spoiledPrincess.doEffect(parameters));
+        Assertions.assertEquals("Null parameter currentPlayer", thrown.getMessage());
+    }
+
+    /**
+     * Verifies that an IllegalArgumentException is thrown when "bag" parameter in parameters is missing.
+     */
+    @Test
+    void doEffectTest_NoBagParameterGiven_ThrowsIllegalArgumentException() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("method", "move");
+        parameters.put("wantedHouse", House.YELLOW);
+        parameters.put("currentPlayer", currentPlayer);
+
+        IllegalArgumentException thrown = Assertions.assertThrows(IllegalArgumentException.class, () -> spoiledPrincess.doEffect(parameters));
+        Assertions.assertEquals("Missing parameter bag", thrown.getMessage());
+    }
+
+    /**
+     * Verifies that an NullPointerException is thrown when "bag" parameter in parameters is null.
+     */
+    @Test
+    void doEffectTest_NullBagParameterGiven_ThrowsNullPointerException() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("method", "move");
+        parameters.put("wantedHouse", House.YELLOW);
+        parameters.put("currentPlayer", currentPlayer);
+        parameters.put("bag", null);
+
+        NullPointerException thrown = Assertions.assertThrows(NullPointerException.class, () -> spoiledPrincess.doEffect(parameters));
+        Assertions.assertEquals("Null parameter bag", thrown.getMessage());
+    }
+
+    /**
+     * Verifies that a NotEnoughStudentsOnCard is thrown when "method" parameter in parameters is "move" and there are not enough students of a wanted house on the card.
+     */
+    @Test
+    void doEffectTest_move_NotEnoughStudentsOnCard_ThrowsNotEnoughStudentsOnCardException() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("method", "getStudents");
+        try {
+            spoiledPrincess.doEffect(parameters);
+        } catch (BagException | NotEnoughStudentsOnCardException | StudentsTableException e) {
+            e.printStackTrace();
+            fail();
+        }
+        HashMap<House, Integer> studentsOnCard = new HashMap<>((HashMap<House, Integer>) parameters.get("studentsOnCard"));
+        int i = 0;
+        House wantedHouse = null;
+        boolean emptyHouseFound = false;
+        do {
+            House h = House.values()[i];
+            if (studentsOnCard.get(h) == 0) {
+                emptyHouseFound = true;
+                wantedHouse = h;
+            }
+            i++;
+        } while (!emptyHouseFound);
+        parameters.replace("method", "move");
+        parameters.put("wantedHouse", wantedHouse);
+        parameters.put("currentPlayer", currentPlayer);
+        parameters.put("bag", bag);
+
+        NotEnoughStudentsOnCardException thrown = Assertions.assertThrows(NotEnoughStudentsOnCardException.class, () -> spoiledPrincess.doEffect(parameters));
+        Assertions.assertEquals("Not enough students of" + wantedHouse + " house on the card", thrown.getMessage());
+    }
+}
