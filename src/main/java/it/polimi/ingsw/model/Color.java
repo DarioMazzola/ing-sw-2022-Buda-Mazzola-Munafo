730807b0041de31cc8a4109174ef38c78b7edfe2
DarioMazzola <dariomazzola@hotmail.com>
2022-05-11 15:14:46 +0200

    Add ANSI Code to Color.java

--- src/main/java/it/polimi/ingsw/model/Color.java
+++ src/main/java/it/polimi/ingsw/model/Color.java
@@ -1,10 +1,30 @@
 package it.polimi.ingsw.model;
 
 /** enum class representing Tower Colors
- * @author Alessio Buda
+ * @author Alessio Buda & Dario Mazzola
  */
 public enum Color {
-    WHITE,
-    GRAY,
-    BLACK
+    WHITE ("WHITE", "\u001B[0m"),
+    GRAY("GRAY","\u001B[37m"),
+    BLACK("BLACK","\u001B[30m");
+
+    public static final String RESET = "\u001B[0m";
+
+    private final String escape;
+    private final String name;
+
+    Color(String name, String escape) {
+        this.name = name;
+        this.escape = escape;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return escape + name + RESET;
+    }
 }
+
