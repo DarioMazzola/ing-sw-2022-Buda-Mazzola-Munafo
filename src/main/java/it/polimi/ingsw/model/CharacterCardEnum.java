33b7049ff388e5313d3c838da2268b74cb6ac575
DarioMazzola <dariomazzola@hotmail.com>
2022-05-11 15:11:25 +0200

    Add JavaDoc to CharacterCardEnum

--- src/main/java/it/polimi/ingsw/model/CharacterCardEnum.java
+++ src/main/java/it/polimi/ingsw/model/CharacterCardEnum.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw.model;
 
+/**
+ *Enumeration representing all the names of the character cards
+ *
+ * @author Dario Mazzola & Gabriele Munafo'
+ */
 public enum CharacterCardEnum {
     FARMER,
     MAGICAL_MAILMAN,
