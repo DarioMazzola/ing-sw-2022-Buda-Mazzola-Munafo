2b9d700376dd9dc7c6c97aab1227bb3b714f555f
Gabriele Munafo <gabriele.munafo@mail.polimi.it>
2022-05-11 13:25:12 +0200

    Add CharacterCardEnum

---
+++ src/main/java/it/polimi/ingsw/model/CharacterCardEnum.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.model;
+
+public enum CharacterCardEnum {
+    FARMER,
+    MAGICAL_MAILMAN,
+    HERB_GRANMA,
+    MINSTREL,
+    MONK,
+    HERALD,
+    CENTAUR,
+    JOLLY,
+    KNIGHT,
+    MUSHROOM_HUNTER,
+    SPOILED_PRINCESS,
+    THIEF
+}
