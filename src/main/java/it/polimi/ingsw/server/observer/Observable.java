62c267e1f742c723d45a4c8adfcadcc55e93d5a1
DarioMazzola <dariomazzola@hotmail.com>
2022-05-16 17:09:12 +0200

    Add Observer interface and Observable class of the Observer Pattern

---
+++ src/main/java/it/polimi/ingsw/server/observer/Observable.java
@@ -0,0 +1,48 @@
+package it.polimi.ingsw.server.observer;
+
+import it.polimi.ingsw.messages.Message;
+import it.polimi.ingsw.messages.command.CommandMessage;
+import it.polimi.ingsw.messages.command.ExpertMode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Observable class. Classes that implements this class may be observed by class that are implementing {@link Observer} interface
+ *
+ * @author Dario Mazzola
+ */
+public class Observable {
+
+    private final List<Observer> observers = new ArrayList<>();
+
+    /**
+     * Adds an observer to the observers' list.
+     *
+     * @param obs the observer to add.
+     */
+    public void addObserver(Observer obs) {
+        observers.add(obs);
+    }
+
+    /**
+     * Removes an observer from the observers' list.
+     *
+     * @param obs the observer to remove.
+     */
+    public void removeObserver(Observer obs) {
+        observers.remove(obs);
+    }
+
+    /**
+     * Notifies all the observers and passes to them a message as a parameter.
+     *
+     * @param message the message to pass to the observers.
+     */
+    protected void notifyObserver(Message message) {
+        for (Observer observer : observers) {
+            observer.update(message);
+        }
+    }
+
+}
---
+++ src/main/java/it/polimi/ingsw/server/observer/Observer.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.server.observer;
+
+import it.polimi.ingsw.messages.Message;
+
+/**
+ * Observer interface. Classes that implement this interface observe classes that are extending Observable interface.
+ * @author Dario Mazzola
+ */
+public interface Observer {
+
+    /**
+     * Update method of the observer pattern.
+     * Notifies all observers that a message has been received
+     * @param message the message received
+     */
+    void update(Message message);
+}
