91874a87163e8778e11c3f7660a96562ef0eeec3
Gabriele Munafo <gabriele.munafo@mail.polimi.it>
2022-05-25 01:30:20 +0200

    Add GamePhase

---
+++ src/main/java/it/polimi/ingsw/controller/GamePhase.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.controller;
+
+public enum GamePhase {
+    CREATE_GAME,
+    NUM_PLAYERS,
+    NICKNAME,
+    SELECT_TEAM,
+    EXPERT_MODE,
+    WIZARD,
+    SELECT_COLOR_TOWER,
+    ASSISTANT_CARD
+}
