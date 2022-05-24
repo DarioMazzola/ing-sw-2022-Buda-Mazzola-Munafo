6912cfffc64357289d2525566246c6f0699d59da
DarioMazzola <dariomazzola@hotmail.com>
2022-05-24 19:44:15 +0200

    Change type in SelectCharacterCard

--- src/main/java/it/polimi/ingsw/messages/answer/SelectCharacterCard.java
+++ src/main/java/it/polimi/ingsw/messages/answer/SelectCharacterCard.java
@@ -4,14 +4,14 @@ import it.polimi.ingsw.model.CharacterCard;
 
 import java.util.List;
 
-import static it.polimi.ingsw.messages.MessageType.ALL_CHARACTER_CARDS;
+import static it.polimi.ingsw.messages.MessageType.SELECT_CHARACTER_CARD;
 
 /**
  * This message is sent from the server to the client to communicate all the character cards in use in this game.
  *
  * @author Dario Mazzola
  */
-public class AllCharacterCards extends AnswerMessage{
+public class SelectCharacterCard extends AnswerMessage{
 
     private final List<CharacterCard> characterCards;
     /**
@@ -19,8 +19,8 @@ public class AllCharacterCards extends AnswerMessage{
      *
      * @param characterCards The list of CharacterCards available for this match
      */
-    public AllCharacterCards(List<CharacterCard> characterCards) {
-        super(ALL_CHARACTER_CARDS);
+    public SelectCharacterCard(List<CharacterCard> characterCards) {
+        super(SELECT_CHARACTER_CARD);
         this.characterCards = characterCards;
     }
 
