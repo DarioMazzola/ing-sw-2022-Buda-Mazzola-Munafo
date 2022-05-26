4540ee4695624a3e05ba90288d98d1a302590e82
Gabriele Munafo <gabriele.munafo@mail.polimi.it>
2022-05-26 19:21:47 +0200

    Add map and Integer in ReducedCharacterCard

--- src/main/java/it/polimi/ingsw/client/ReducedCharacterCard.java
+++ src/main/java/it/polimi/ingsw/client/ReducedCharacterCard.java
@@ -2,6 +2,12 @@ package it.polimi.ingsw.client;
 
 import it.polimi.ingsw.model.CharacterCard;
 import it.polimi.ingsw.model.CharacterCardEnum;
+import it.polimi.ingsw.model.House;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static it.polimi.ingsw.model.CharacterCardEnum.*;
 
 /**
  * Class that represents the ReducedGameModel
@@ -13,6 +19,8 @@ public class ReducedCharacterCard {
     private final String cardName;
     private final CharacterCardEnum type;
     private final String description;
+    private final Map<House, Integer> houseMap;
+    private Integer noEntryTile;
 
     public ReducedCharacterCard(CharacterCard c){
         cost = c.getCost();
@@ -20,6 +28,37 @@ public class ReducedCharacterCard {
         cardName = c.toString();
         type = c.getType();
         description = c.getDescription();
+
+        if (c.getType() == MONK || c.getType() == JOLLY || c.getType() == SPOILED_PRINCESS) {
+            houseMap = new HashMap<>();
+            Map<String, Object> parameters = new HashMap<>();
+
+            parameters.put("method", "getStudents");
+
+            try {
+                c.doEffect(parameters);
+                houseMap.putAll((HashMap<House, Integer>) parameters.get("studentsOnCard"));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            houseMap = null;
+        }
+
+        if (c.getType() == HERB_GRANMA){
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("Method", "getNoEntryTileNumber");
+            try {
+                c.doEffect(parameters);
+                noEntryTile = (int) parameters.get("noEntryTileNumber");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            noEntryTile = null;
+        }
     }
 
     public String getCardName(){
@@ -40,5 +79,13 @@ public class ReducedCharacterCard {
 
     public boolean isInUse(){
         return inUse;
+    }
+
+    public Integer getNoEntryTile(){
+        return noEntryTile;
+    }
+
+    public Map<House, Integer> getHouseMap(){
+        return houseMap;
     }
 }
