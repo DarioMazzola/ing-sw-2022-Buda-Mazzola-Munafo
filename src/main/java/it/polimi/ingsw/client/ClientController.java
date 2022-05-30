abf75b68301829b47448ab7715152f3022dd12ba
alessio-buda <100711335+alessio-buda@users.noreply.github.com>
2022-05-30 09:13:09 +0200

    Fix imports in ClientController

--- src/main/java/it/polimi/ingsw/client/ClientController.java
+++ src/main/java/it/polimi/ingsw/client/ClientController.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.lang.String;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
