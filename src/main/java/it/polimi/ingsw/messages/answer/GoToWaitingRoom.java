2f056ff7b7dd81a5b225f5d51c41fc268e0ebcaf
DarioMazzola <dariomazzola@hotmail.com>
2022-05-30 18:35:40 +0200

    Create message GoToLobby and update GoToWaitingRoom

--- src/main/java/it/polimi/ingsw/messages/answer/GoToWaitingRoom.java
+++ src/main/java/it/polimi/ingsw/messages/answer/GoToWaitingRoom.java
@@ -1,13 +1,11 @@
 package it.polimi.ingsw.messages.answer;
 
-
 import static it.polimi.ingsw.messages.MessageType.GO_TO_WAITING_ROOM;
 
 public class GoToWaitingRoom extends AnswerMessage{
 
     /**
-     * Abstract class representing a message that is sent from the server to the client
-     *
+     * Class constructor.
      */
     public GoToWaitingRoom() {
         super(GO_TO_WAITING_ROOM);
