16dec797f48ee21841c6c576eae87aa81b4768ac
DarioMazzola <dariomazzola@hotmail.com>
2022-05-30 09:59:10 +0200

    Add ascii color to House enum

--- src/main/java/it/polimi/ingsw/model/House.java
+++ src/main/java/it/polimi/ingsw/model/House.java
@@ -1,12 +1,33 @@
 package it.polimi.ingsw.model;
 
-/** enum class representing Students' and Professors' Houses
- * @author Alessio Buda
+/**
+ * enum class representing Students' and Professors' Houses
+ *
+ * @author Alessio Buda & Dario Mazzola
  */
 public enum House {
-    YELLOW,
-    BLUE,
-    GREEN,
-    RED,
-    PINK
+    YELLOW("YELLOW", "\u001b[33m"),
+    BLUE("BLUE", "\u001b[34m"),
+    GREEN("GREEN", "\u001b[32m"),
+    RED("RED", "\u001b[31m"),
+    PINK("PINK", "\u001b[35;1m");
+
+    public static final String RESET = "\u001B[0m";
+
+    private final String escape;
+    private final String name;
+
+    House(String name, String escape) {
+        this.name = name;
+        this.escape = escape;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    public String getColouredHouse() {
+        return escape + name + RESET;
+    }
 }
