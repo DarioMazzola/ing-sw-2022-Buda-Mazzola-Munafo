79e652c3b1367f43b63149f26be977bfc4748262
alessio-buda <100711335+alessio-buda@users.noreply.github.com>
2022-06-04 15:01:29 +0200

    Add ChosenChat message

---
+++ src/main/java/it/polimi/ingsw/messages/command/ChosenChat.java
@@ -0,0 +1,28 @@
+package it.polimi.ingsw.messages.command;
+
+import it.polimi.ingsw.messages.MessageType;
+
+/**
+ * ChosenChat message is sent from the client to the server
+ * to communicate whether the player wants to allow communication between team members.
+ *
+ * @author Alessio Buda
+ */
+public class ChosenChat extends CommandMessage {
+    private final boolean chat;
+
+    /**
+     * Class constructor.
+     *
+     * @param nickname the player's nickname
+     * @param chat true if the player wants to allow communication between team members
+     */
+    public ChosenChat(String nickname, boolean chat) {
+        super(MessageType.CHOSEN_CHAT, nickname);
+        this.chat = chat;
+    }
+
+    public boolean isChat() {
+        return chat;
+    }
+}
