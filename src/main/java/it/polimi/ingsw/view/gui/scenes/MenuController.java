f2c13a894bd6b8df0a3d7f6507ee91e974c5dd4f
DarioMazzola <dariomazzola@hotmail.com>
2022-06-06 18:32:47 +0200

    Add MenuController class

---
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/MenuController.java
@@ -0,0 +1,34 @@
+package it.polimi.ingsw.view.gui.scenes;
+
+import it.polimi.ingsw.observer.ViewObservable;
+import it.polimi.ingsw.view.gui.SceneController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class MenuController extends ViewObservable implements SceneInterface{
+
+    @FXML
+    private AnchorPane scenePane;
+
+    public void exit(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Logout");
+        alert.setHeaderText("You are about to logout!");
+        alert.setContentText("Are you sure?");
+        Stage stage;
+        if(alert.showAndWait().orElse(null) == ButtonType.OK) {
+            stage = (Stage) scenePane.getScene().getWindow();
+            stage.close();
+        } stage = (Stage) scenePane.getScene().getWindow();
+        stage.close(); //per chiudere l’app
+    }
+
+    public void play(ActionEvent event) {
+        SceneController.changeRootPane(observers, event, "connectionScene.fxml");
+    }
+
+}
