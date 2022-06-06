0381f83b73f4598a64ec2d06c2402ba4010c41e4
DarioMazzola <dariomazzola@hotmail.com>
2022-06-06 18:26:04 +0200

    Add SceneInterface interface

---
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/SceneInterface.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.view.gui.scenes;
+
+/**
+ * Scene interface. Each controller inherits from this interface so that
+ * it is easier to manage the scene change
+ *
+ * @author Dario Mazzola
+ */
+public interface SceneInterface {
+}
