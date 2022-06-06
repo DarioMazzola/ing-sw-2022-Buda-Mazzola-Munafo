09d8bf5a1a7659b3c57afb00934feb7297e06c69
DarioMazzola <dariomazzola@hotmail.com>
2022-06-06 18:32:10 +0200

    Add ConnectionSceneController class

---
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/ConnectionSceneController.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw.view.gui.scenes;
+
+import it.polimi.ingsw.observer.ViewObservable;
+import it.polimi.ingsw.view.gui.SceneController;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ConnectionSceneController extends ViewObservable implements SceneInterface {
+
+    public Button login;
+    @FXML
+    private TextField serverIpBox;
+    @FXML
+    private TextField serverPortBox;
+
+    public void initialize() {
+        serverIpBox.setText("127.0.0.1");
+        serverPortBox.setText("1234");
+    }
+
+    public void login(ActionEvent event) {
+        String ip = serverIpBox.getText();
+        int port = Integer.parseInt(serverPortBox.getText());
+        notifyObserver(observers -> observers.onInit(ip, port));
+        SceneController.changeRootPane(observers, event, "loginScene.fxml");
+    }
+}
