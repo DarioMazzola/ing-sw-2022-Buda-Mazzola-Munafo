4847a6f8a2add0138a1f5aa18bc0d1b8fa684421
DarioMazzola <dariomazzola@hotmail.com>
2022-06-18 12:33:08 +0200

    Add LobbySceneController

---
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/LobbySceneController.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.view.gui.scenes;
+
+import it.polimi.ingsw.observer.ViewObservable;
+
+public class LobbySceneController extends ViewObservable implements SceneInterface {
+}
