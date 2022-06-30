88e3076d59e9708a531ccdfc5a6f372ea1e25c6d
DarioMazzola <dariomazzola@hotmail.com>
2022-06-30 12:04:35 +0200

    Add JavaDoc to LoginSceneController

--- src/main/java/it/polimi/ingsw/view/gui/scenes/LoginSceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/LoginSceneController.java
@@ -12,6 +12,9 @@ import java.io.IOException;
 
 import static javafx.scene.control.Alert.AlertType.*;
 
+/**
+ * Scene controller asking the player to enter the nickname with which he wants to take part in the game
+ */
 public class LoginSceneController extends ViewObservable implements SceneInterface {
 
     @FXML
@@ -19,10 +22,14 @@ public class LoginSceneController extends ViewObservable implements SceneInterfa
     @FXML
     private Button playButton;
 
-
+    /**
+     * Handles the event fired when the player clicks on the select button.
+     *
+     * @param event the event fired.
+     */
     public void onPlayButton(ActionEvent event) {
         String nickname = usernameBox.getText();
-        if(nickname == null || nickname.isEmpty()){
+        if (nickname == null || nickname.isEmpty()) {
             Alert alert = new Alert(ERROR, "", ButtonType.OK);
             alert.setTitle("Invalid nickname");
             alert.setHeaderText("Empty nickname! Please insert a nickname\nthat is at least 1 character long");
