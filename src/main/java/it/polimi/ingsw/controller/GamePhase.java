9bba161522e27cf48a4969e3b6a5350dd31d0fe8
DarioMazzola <dariomazzola@hotmail.com>
2022-06-30 15:39:40 +0200

    Add and update JavaDoc in GamePhase

--- src/main/java/it/polimi/ingsw/controller/GamePhase.java
+++ src/main/java/it/polimi/ingsw/controller/GamePhase.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw.controller;
 
+/**
+ * Class enumeration representing all the possible phases in the game
+ *
+ * @author Dario Mazzola and Gabriele Munafo'
+ */
 public enum GamePhase {
     CREATE_GAME,
     NUM_PLAYERS,
