9f7bc42b5405df5b16583a4e6011ba5528bab30f
DarioMazzola <dariomazzola@hotmail.com>
2022-06-30 12:01:04 +0200

    Add JavaDoc to ConnectionSceneController

--- src/main/java/it/polimi/ingsw/view/gui/scenes/ConnectionSceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/ConnectionSceneController.java
@@ -7,6 +7,12 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
+/**
+ * Scene that gives the player the ability to connect to the server. The fields are already pre-filled,
+ * but the player can decide to change them as he/she likes.
+ *
+ * @author Dario Mazzola
+ */
 public class ConnectionSceneController extends ViewObservable implements SceneInterface {
 
     public Button login;
@@ -15,11 +21,17 @@ public class ConnectionSceneController extends ViewObservable implements SceneIn
     @FXML
     private TextField serverPortBox;
 
+    @FXML
     public void initialize() {
         serverIpBox.setText("127.0.0.1");
         serverPortBox.setText("1234");
     }
 
+    /**
+     * Handles the event fired when the player clicks on the select button.
+     *
+     * @param event the event fired
+     */
     public void login(ActionEvent event) {
 
         SceneController sceneController = new SceneController();
