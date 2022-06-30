e36b2d6535fc94436e704fb4edb47fcc36548284
DarioMazzola <dariomazzola@hotmail.com>
2022-06-30 12:02:36 +0200

    Add JavaDoc to LobbySceneController

--- src/main/java/it/polimi/ingsw/view/gui/scenes/LobbySceneController.java
+++ src/main/java/it/polimi/ingsw/view/gui/scenes/LobbySceneController.java
@@ -2,5 +2,11 @@ package it.polimi.ingsw.view.gui.scenes;
 
 import it.polimi.ingsw.observer.ViewObservable;
 
+/**
+ * Scene controller representing the lobby where the player has to wait for
+ * other players to connect before starting to play.
+ *
+ * @author Dario Mazzola
+ */
 public class LobbySceneController extends ViewObservable implements SceneInterface {
 }
