99e70feac1775cf8c0453cbef9105f9bc96faeaf
DarioMazzola <dariomazzola@hotmail.com>
2022-06-30 15:40:18 +0200

    Add and update JavaDoc in GameState

--- src/main/java/it/polimi/ingsw/controller/GameState.java
+++ src/main/java/it/polimi/ingsw/controller/GameState.java
@@ -1,5 +1,10 @@
 package it.polimi.ingsw.controller;
 
+/**
+ * Class enumeration representing all the possible states in the game
+ *
+ * @author Dario Mazzola and Gabriele Munafo'
+ */
 public enum GameState {
     SETUP,
     START,
