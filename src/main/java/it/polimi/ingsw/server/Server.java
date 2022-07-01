83d225a9b3dfe42989a8832a9258b34acbd6be6a
DarioMazzola <dariomazzola@hotmail.com>
2022-07-01 10:01:19 +0200

    Change disconnection management in Server

--- src/main/java/it/polimi/ingsw/server/Server.java
+++ src/main/java/it/polimi/ingsw/server/Server.java
@@ -50,7 +50,7 @@ public class Server {
      * If there is a game in progress or the game is already started, the client will receive an
      * error message telling him that he cannot play.
      *
-     * @param message the message sent by the client.
+     * @param message       the message sent by the client.
      * @param clientHandler the ClientHandler associated with the client.
      */
     public void addClient(CommandMessage message, ClientHandler clientHandler) {
@@ -59,16 +59,15 @@ public class Server {
         Persistence persistence = new Persistence();
         //if the player is the first, tries to restore the game
         synchronized (lock) {
-            if(clientHandlerMap.isEmpty()) {
-                if(persistence.matchExists()) {
+            if (clientHandlerMap.isEmpty()) {
+                if (persistence.matchExists()) {
                     turnController = persistence.restoreData();
                     restoredQueue = turnController.getQueue();
                     turnController.initializeVirtualViewMap();
                     restored = true;
-                }
-                else
+                } else
                     turnController = new TurnController();
-            } else if (turnController.reset()){
+            } else if (turnController.reset()) {
                 turnController = new TurnController();
                 restored = false;
                 firstHandler = null;
@@ -82,9 +81,8 @@ public class Server {
                     turnController.loginHandler(sender, clientHandler);
                     //asks the player whether to restore the game
                     turnController.selectRestore(sender);
-                }
-                else { //if the player is not the first
-                    if(turnController.checkLoginNickname(sender, clientHandler)) {
+                } else { //if the player is not the first
+                    if (turnController.checkLoginNickname(sender, clientHandler)) {
                         clientHandlerMap.put(sender, clientHandler);
                         turnController.loginHandler(sender, clientHandler);
                         //if the player is the last
@@ -101,13 +99,11 @@ public class Server {
                             turnController.goToLobby(sender);
                     }
                 }
-            }
-            else if (!turnController.isGameStarted()){ // the player was not present in the saved game, he/she cannot play
+            } else if (!turnController.isGameStarted()) { // the player was not present in the saved game, he/she cannot play
                 turnController.loginHandler(sender, clientHandler);
                 turnController.showError(sender, GAME_RESTORED_NICKNAME_NOT_PRESENT.toString());
                 turnController.sendNickname(sender);
-            }
-            else {
+            } else {
                 turnController.removeFromVirtualViewMap(sender);
                 clientHandlerMap.remove(sender);
                 turnController.showError(clientHandler, GAME_ALREADY_STARTED.toString());
@@ -115,26 +111,26 @@ public class Server {
         }
 
         // if the game has not already started
-        else if (! turnController.isGameStarted()) {
+        else if (!turnController.isGameStarted()) {
             // if he/she is the first player
             if (clientHandlerMap.isEmpty() || (firstHandler != null && firstHandler.equals(clientHandler))) {
-                if(firstHandler == null) {
+                if (firstHandler == null) {
                     clientHandlerMap.put(sender, clientHandler);
                     firstHandler = clientHandler;
                 }
                 if (!turnController.checkLoginNickname(sender, clientHandler))
                     return;
-                if(turnController.getPhase() == GamePhase.CREATE_GAME)
+                if (turnController.getPhase() == GamePhase.CREATE_GAME)
                     turnController.loginHandler(sender, clientHandler);
                 turnController.selectMainPhase(message);
             }
             // if he/she is not the first player
-            else if(turnController.checkLoginNickname(sender, clientHandler)){
+            else if (turnController.checkLoginNickname(sender, clientHandler)) {
                 turnController.loginHandler(sender, clientHandler);
                 clientHandlerMap.put(sender, clientHandler);
 
                 // if the game model has not been created or the maximum number of players has not been reached
-                if (! turnController.gameModelExists() || !(turnController.getVirtualViewMap().size() == turnController.getNumPlayers())) {
+                if (!turnController.gameModelExists() || !(turnController.getVirtualViewMap().size() == turnController.getNumPlayers())) {
                     turnController.goToLobby(sender);
                 }
                 // if the game model has been created
@@ -142,8 +138,7 @@ public class Server {
                     turnController.checkIfFull(restored);
                 }
             }
-        }
-        else {
+        } else {
             turnController.removeFromVirtualViewMap(sender);
             clientHandlerMap.remove(sender);
             turnController.showError(clientHandler, GAME_ALREADY_STARTED.toString());
@@ -178,16 +173,17 @@ public class Server {
      * Manages the first player's decision if there is a game saved on the server memory.
      * If the player has decided not to resume the saved game then a new one will be created,
      * otherwise the game will resume where it left off.
-     * @param message the message sent by the client that represents his/her decision.
+     *
+     * @param message       the message sent by the client that represents his/her decision.
      * @param clientHandler the client handler associated to the client
      */
-    public void restoreGame(CommandMessage message, ClientHandler clientHandler){
-        synchronized (lock){
+    public void restoreGame(CommandMessage message, ClientHandler clientHandler) {
+        synchronized (lock) {
             turnController.setRestoreDecisionTaken();
         }
         String sender = message.getNickname();
         //if the player does not want to restore the game
-        if(! ((ChosenRestoreGame)message).getToRestore()) {
+        if (!((ChosenRestoreGame) message).getToRestore()) {
 
             Persistence persistence = new Persistence();
             persistence.delete();
@@ -198,8 +194,8 @@ public class Server {
 
             turnController.loginHandler(sender, clientHandler);
 
-            for(String n : clientHandlerMap.keySet()){
-                if(! n.equals(sender))
+            for (String n : clientHandlerMap.keySet()) {
+                if (!n.equals(sender))
                     turnController.loginHandler(n, clientHandlerMap.get(n));
             }
             turnController.selectMainPhase(message);
@@ -219,9 +215,10 @@ public class Server {
     /**
      * Forwards the chat message to the turn controller to send it
      * to the teammate of the player who sent it.
+     *
      * @param message the chat message sent and to forward
      */
-    public void chat(CommandMessage message){
+    public void chat(CommandMessage message) {
         turnController.chat(message);
     }
 
@@ -231,14 +228,17 @@ public class Server {
      *
      * @param clientHandler the clientHandler of the player who disconnected
      */
-    public void onDisconnection(ClientHandler clientHandler){
+    public void onDisconnection(ClientHandler clientHandler) {
         String playerDisconnected = getNicknameFromClientHandler(clientHandler);
-        for(String nickname : clientHandlerMap.keySet()) {
-            if(! nickname.equals(playerDisconnected)) {
+        for (String nickname : clientHandlerMap.keySet()) {
+            if (!nickname.equals(playerDisconnected)) {
                 String interruptedBy = playerDisconnected + DISCONNECTED;
                 turnController.endGameDisconnection(nickname, interruptedBy);
             }
         }
+        turnController = new TurnController();
+        restored = false;
+        firstHandler = null;
         clientHandlerMap = Collections.synchronizedMap(new HashMap<>());
     }
 
@@ -248,7 +248,7 @@ public class Server {
      *
      * @param nickname the nickname of the player to send the available actions to
      */
-    public void resendAvailableActions(String nickname){
+    public void resendAvailableActions(String nickname) {
         turnController.resendActionPhase(nickname);
     }
 
@@ -263,8 +263,8 @@ public class Server {
 
         String nickname = null;
 
-        for(String s : clientHandlerMap.keySet()){
-            if(clientHandlerMap.get(s).equals(clientHandler)){
+        for (String s : clientHandlerMap.keySet()) {
+            if (clientHandlerMap.get(s).equals(clientHandler)) {
                 nickname = s;
             }
         }
